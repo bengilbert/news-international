f3c0ef8bf02cd18245c7871582373c984157f96e
Ben Gilbert <ben.gilbert2@gmail.com>
2013-07-03 13:03:57 +0100

    customised main class for musiccubes requirements.  Now loads rover commands from the command line arguments not a file.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,199 +4,95 @@ import gilbert.challenge.Position;
 import gilbert.challenge.Robot;
 import gilbert.challenge.Command.MOVEMENT_COMMAND;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Command line wrapper which loads a file adhering to the following format:
+ * Command line wrapper which parses command line arguments which are a series
+ * of characters representing the rovers movements:
  * 
- * The first line of input is the upper-right coordinates of the plateau, the
- * lower-left coordinates are assumed to be 0,0. The rest of the input is
- * information pertaining to the robots that have been deployed. Each robot has
- * two lines of input. The first line gives the robot's position, and the second
- * line is a series of instructions telling the robot how to explore the
- * plateau. 
+ * R - rotate rover right 90 degrees L - rotate rover left 90 degrees F - move
+ * rover forward 1 unit.
  * 
- * The final position of all robots are displayed after all commands have been
- * executed.
+ * All commands must be grouped together. e.g. RFLFFRF.
+ * 
+ * The rover is assumed to start at position 0,0 to be north facing.
+ * 
+ * The rover's final grid position is displayed at the end of execution
  * 
  */
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		String commandFileName = null;
 
 		/*
 		 * Got arguments?
 		 */
 		if (args.length > 0) {
 
-			commandFileName = args[0];
+			Controller controller = new Controller(4, 4);
 
 			/*
-			 * Attempt to read in specified file
+			 * Parse robot initial position and movements. Robots are parsed
+			 * individually and it is assumed that each robot command is correct
+			 * and contains an initial position line as well as a line of
+			 * movement commands.
 			 */
-			BufferedReader file = null;
-			try {
-				file = new BufferedReader(new FileReader(commandFileName));
-			} catch (FileNotFoundException e) {
+			List<Command> robotMovementCommands = new ArrayList<Command>();
+
+			Command command = new Command(0, 0, Position.NORTH);
+
+			// extract movement commands
+			String movementCommands = args[0];
+
+			// ignore case differences in input
+			movementCommands = movementCommands.toUpperCase();
+
+			if (movementCommands == null || movementCommands.isEmpty()) {
 				System.out
-						.println("ERROR: command file not found, please check filename and try again.");
+						.println("ERROR: Robots movement commands must be specified");
 				System.exit(1);
 			}
 
+			for (int i = 0; i < movementCommands.length(); i++) {
+				char commandCode = movementCommands.charAt(i);
+				switch (commandCode) {
+				case 'F':
+					command.addCommand(MOVEMENT_COMMAND.MOVE);
+					break;
+				case 'L':
+					command.addCommand(MOVEMENT_COMMAND.ROTATE_LEFT);
+					break;
+				case 'R':
+					command.addCommand(MOVEMENT_COMMAND.ROTATE_RIGHT);
+					break;
+				}
+
+			}
+			robotMovementCommands.add(command);
+
 			/*
-			 * Parse file in a primitive manner, creating commands to send to
-			 * the controller as the file is parsed
+			 * Execute robot commands
 			 */
-			Controller controller = null;
-			try {
-				/*
-				 * Extract the plateau maximum dimensions
-				 */
-				String plateauDimensionLine = file.readLine();
-				String[] dimensions = plateauDimensionLine.split(" ");
+			controller.executeCommands(robotMovementCommands);
 
-				if (dimensions.length < 2) {
-					System.out
-							.println("ERROR:  first line of command file must specify dimensions.  eg 10 10.");
-					System.exit(1);
-				}
+			/*
+			 * Determine robots final position and heading
+			 */
+			List<Robot> robots = controller.getRobots();
 
-				int maxX = 0;
-				int maxY = 0;
-				try {
-					maxX = Integer.parseInt(dimensions[0]);
-					maxY = Integer.parseInt(dimensions[1]);
-				} catch (NumberFormatException ex) {
-					System.out
-							.println("ERROR: plateau dimensions must be numeric");
-					System.exit(1);
-				}
+			for (Robot robot : robots) {
+				int finalX = robot.getX();
+				int finalY = robot.getY();
 
-				controller = new Controller(maxX, maxY);
+				System.out.println("Rover's final position is (" + finalX + ","
+						+ finalY + ")");
 
-				/*
-				 * Parse robot initial position and movements. Robots are parsed
-				 * individually and it is assumed that each robot command is
-				 * correct and contains an initial position line as well as a
-				 * line of movement commands.
-				 */
-				List<Command> robotMovementCommands = new ArrayList<Command>();
-				String initialPosition = null;
-				while ((initialPosition = file.readLine()) != null) {
-
-					/*
-					 * Extract robots initial position
-					 */
-					String[] position = initialPosition.split(" ");
-					if (position.length < 3) {
-						System.out
-								.println("ERROR: Robots initial position must be in correct format. X Y Heading");
-						System.exit(1);
-					}
-
-					int initialX = 0;
-					int initialY = 0;
-					int initialHeading = 0;
-
-					try {
-						initialX = Integer.parseInt(position[0]);
-						initialY = Integer.parseInt(position[1]);
-						switch (position[2].charAt(0)) {
-						case 'N':
-							initialHeading = Position.NORTH;
-							break;
-						case 'E':
-							initialHeading = Position.EAST;
-							break;
-						case 'S':
-							initialHeading = Position.SOUTH;
-							break;
-						case 'W':
-							initialHeading = Position.WEST;
-							break;
-						default:
-							System.out
-									.println("ERROR: Unknown initial heading of "
-											+ position[2]
-											+ ".  Must be N, E, S or W.");
-							System.exit(1);
-						}
-					} catch (NumberFormatException ex) {
-						System.out
-								.println("ERROR: Unable to parse Robots initial position.  Please verify format is correct.");
-						System.exit(1);
-					}
-
-					Command command = new Command(initialX, initialY,
-							initialHeading);
-
-					// extract movement commands
-					String movementCommands = file.readLine();
-
-					// ignore case differences in input
-					movementCommands = movementCommands.toUpperCase();
-
-					if (movementCommands == null || movementCommands.isEmpty()) {
-						System.out
-								.println("ERROR: Robots movement commands must be specified");
-						System.exit(1);
-					}
-
-					for (int i = 0; i < movementCommands.length(); i++) {
-						char commandCode = movementCommands.charAt(i);
-						switch (commandCode) {
-						case 'M':
-							command.addCommand(MOVEMENT_COMMAND.MOVE);
-							break;
-						case 'L':
-							command.addCommand(MOVEMENT_COMMAND.ROTATE_LEFT);
-							break;
-						case 'R':
-							command.addCommand(MOVEMENT_COMMAND.ROTATE_RIGHT);
-							break;
-						}
-
-					}
-					robotMovementCommands.add(command);
-
-				}
-
-				/*
-				 * Execute robot commands
-				 */
-				controller.executeCommands(robotMovementCommands);
-
-				/*
-				 * Determine robots final position and heading
-				 */
-				List<Robot> robots = controller.getRobots();
-
-				for (Robot robot : robots) {
-					int finalX = robot.getX();
-					int finalY = robot.getY();
-					String finalBearing = robot.getBearing();
-
-					System.out.println("" + finalX + " " + finalY + " "
-							+ finalBearing);
-
-				}
-
-			} catch (IOException e) {
-				System.out
-						.println("ERROR: Unexpected error in command file.  Please verify format is correct.");
-			} finally {
-				file.close();
 			}
 
 		} else {
-			System.out
-					.println("Command file not specifed.  Please enter name of command file.");
+			System.out.println("Movement commands not specified e.g. RFLFFRF.");
 		}
 
 	}
